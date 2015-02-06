a05aec66e81b9f2fdf68b5d9f8072a0af6d63e1d
Billy Ceskavich <bceskavich@gmail.com>
2015-02-06 11:11:37 -0500

    Content for NEXISx.

---
+++ bubble.R
@@ -0,0 +1,16 @@
+# Load the data
+crime <- read.csv("http://datasets.flowingdata.com/crimeRatesByState2005.tsv", header=TRUE, sep="\t")
+
+# Set bubble radius based on pop size
+radius <- sqrt(crime$population / pi)
+
+# Plot bubble chart
+symbols(crime$murder, crime$burglary,
+  circles=radius,
+  inches=0.35,
+  fg="white",
+  bg="red",
+  xlab="Murder Rate",
+  ylab="Burglary Rate")
+
+text(crime$murder, crime$burglary, crime$state, cex=0.5)
--- scatter.R
+++ scatter.R
@@ -1,21 +1,12 @@
-# NEXIS Seminar R Example
-#
-# Bar Chart
-
 # Create the data frame
 cities <- c('Buffalo', 'Syrause', 'Binghamton', 'Rochester', 'Albany')
 snow <- c(75.5, 60.1, 58.7, 54.2, 52.3)
 snowLevels <- data.frame(cities, snow)
 
-print(snowLevels)
-
-# Render a barplot with the modified data
-barplot(snowLevels$snow,
-  names.arg=snowLevels$cities,
-  col='#821122',
-  space=0.3,
-  border=NA,
+plot(snowLevels$snow,
+  type="p",
+  ylim=c(0, 80),
   xlab="Cities",
-  ylab="Snow Levels (Inches)",
+  ylab="Snow Levels (inches)",
   main="Upstate New York Snow Totals (2015), by City"
 )
